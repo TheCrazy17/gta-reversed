01a8018f82053577d719a8efc261d780fcfe92a9
TheCrazy17 <fedehromero@outlook.com>
2026-09-03 16:32:23 -0300

    re(CTaskComplexPlayHandSignalAnim): Reverse GetAnimIdForPed, Clone

--- source/game_sa/Tasks/TaskTypes/TaskComplexPlayHandSignalAnim.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexPlayHandSignalAnim.cpp
@@ -8,10 +8,10 @@ void CTaskComplexPlayHandSignalAnim::InjectHooks() {
     RH_ScopedInstall(Constructor, 0x61B2B0);
     RH_ScopedInstall(Destructor, 0x61BDF0);
 
-    RH_ScopedInstall(GetAnimIdForPed, 0x61B460, {.reversed = false});
+    RH_ScopedInstall(GetAnimIdForPed, 0x61B460);
     RH_ScopedInstall(CreateSubTask, 0x61B2F0, {.reversed = false});
 
-    RH_ScopedVMTInstall(Clone, 0x61BA00, {.reversed = false});
+    RH_ScopedVMTInstall(Clone, 0x61BA00);
     RH_ScopedVMTInstall(GetTaskType, 0x61B2E0);
     RH_ScopedVMTInstall(CreateNextSubTask, 0x61B570, {.reversed = false});
     RH_ScopedVMTInstall(CreateFirstSubTask, 0x61B4F0, {.reversed = false});
@@ -60,7 +60,14 @@ CTaskComplexPlayHandSignalAnim::~CTaskComplexPlayHandSignalAnim() {
 
 // 0x61B460
 AnimationId CTaskComplexPlayHandSignalAnim::GetAnimIdForPed(CPed* ped) {
-    return plugin::CallAndReturn<AnimationId, 0x61B460, CPed*>(ped);
+    switch (ped->m_nPedType) {
+    case PED_TYPE_GANG1: return ANIM_ID_GSIGN1; // Ballas
+    case PED_TYPE_GANG2: return ANIM_ID_GSIGN2; // Grove Street Families
+    case PED_TYPE_GANG3: return ANIM_ID_GSIGN3; // Los Santos Vagos
+    case PED_TYPE_GANG5: return ANIM_ID_GSIGN5; // Da Nang Boys
+    case PED_TYPE_GANG8: return ANIM_ID_GSIGN4; // Varrio Los Aztecas
+    default:             return ANIM_ID_UNDEFINED;
+    }
 }
 
 // 0x61B2F0
@@ -70,7 +77,10 @@ CTask* CTaskComplexPlayHandSignalAnim::CreateSubTask(eTaskType taskType) {
 
 // 0x61BA00
 CTask* CTaskComplexPlayHandSignalAnim::Clone() const {
-    return plugin::CallMethodAndReturn<CTask*, 0x61BA00, const CTaskComplexPlayHandSignalAnim*>(this);
+    auto* clone = new CTaskComplexPlayHandSignalAnim();
+    clone->m_animationId = m_animationId;
+    clone->m_AnimBlenDelta = m_AnimBlenDelta;
+    return clone;
 }
 
 // 0x61B570
