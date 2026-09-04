1baa6e97e9729507b9a53f5814ab7c56e9d14564
TheCrazy17 <fedehromero@outlook.com>
2026-09-04 17:18:15 -0300

    re(CTaskComplexDestroyCar): Reverse ControlSubTask

--- source/game_sa/Tasks/TaskTypes/TaskComplexDestroyCar.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexDestroyCar.cpp
@@ -19,7 +19,7 @@ void CTaskComplexDestroyCar::InjectHooks() {
     RH_ScopedVMTInstall(MakeAbortable, 0x621C80);
     RH_ScopedVMTInstall(CreateNextSubTask, 0x62D9E0);
     RH_ScopedVMTInstall(CreateFirstSubTask, 0x62DA90);
-    RH_ScopedVMTInstall(ControlSubTask, 0x6288C0, { .reversed = false });
+    RH_ScopedVMTInstall(ControlSubTask, 0x6288C0);
 }
 
 // 0x621C00
@@ -94,5 +94,46 @@ CTask* CTaskComplexDestroyCar::CreateSubTask(eTaskType taskType, CPed* ped) {
 
 // 0x6288C0
 CTask* CTaskComplexDestroyCar::ControlSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x6288C0, CTaskComplexDestroyCar*, CPed*>(this, ped);
+    if (!m_VehicleToDestroy || m_VehicleToDestroy->m_fHealth <= 0.0f) {
+        return nullptr;
+    }
+    if (m_arg0) {
+        return CreateFirstSubTask(ped);
+    }
+
+    eTaskType newSubTaskType;
+    switch (m_pSubTask->GetTaskType()) {
+    case TASK_COMPLEX_DESTROY_CAR_MELEE:
+        if (ped->GetActiveWeapon().IsTypeMelee()) {
+            return m_pSubTask;
+        }
+        newSubTaskType = TASK_COMPLEX_DESTROY_CAR_ARMED;
+        break;
+    case TASK_COMPLEX_DESTROY_CAR_ARMED: {
+        if (!ped->IsCurrentlyUnarmed() || ped->IsPlayer()) {
+            return m_pSubTask;
+        }
+
+        auto slot = 0u;
+        for (; slot < NUM_WEAPON_SLOTS; slot++) {
+            if (ped->m_aWeapons[slot].m_Type > WEAPON_UNARMED) {
+                ped->SetCurrentWeapon((int32)slot);
+                break;
+            }
+        }
+        if (slot != NUM_WEAPON_SLOTS) {
+            return m_pSubTask; // Found & equipped a weapon - stay in armed mode
+        }
+
+        ped->SetCurrentWeapon(0);
+        newSubTaskType = TASK_COMPLEX_DESTROY_CAR_MELEE;
+        break;
+    }
+    default:
+        return m_pSubTask;
+    }
+
+    return m_pSubTask->MakeAbortable(ped, ABORT_PRIORITY_URGENT)
+        ? CreateSubTask(newSubTaskType, ped)
+        : m_pSubTask;
 }
