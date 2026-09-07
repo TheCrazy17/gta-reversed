5925cb518c365eb51ed5cef7165bd10c740a3806
TheCrazy17 <fedehromero@outlook.com>
2026-09-07 15:54:27 -0300

    re(CTaskComplexSignalAtPed): Reverse CreateNextSubTask

--- source/game_sa/Tasks/TaskTypes/TaskComplexSignalAtPed.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexSignalAtPed.cpp
@@ -1,5 +1,8 @@
 #include "StdInc.h"
 #include "TaskComplexSignalAtPed.h"
+#include "TaskSimpleStandStill.h"
+#include "TaskSimpleDoHandSignal.h"
+#include "TaskSimpleRunAnim.h"
 
 void CTaskComplexSignalAtPed::InjectHooks() {
     RH_ScopedVirtualClass(CTaskComplexSignalAtPed, 0x86fa8c, 11);
@@ -10,7 +13,7 @@ void CTaskComplexSignalAtPed::InjectHooks() {
 
     RH_ScopedVMTInstall(Clone, 0x662140);
     RH_ScopedVMTInstall(GetTaskType, 0x660AA0);
-    RH_ScopedVMTInstall(CreateNextSubTask, 0x660B30, {.reversed = false});
+    RH_ScopedVMTInstall(CreateNextSubTask, 0x660B30);
     RH_ScopedVMTInstall(CreateFirstSubTask, 0x660CC0, {.reversed = false});
     RH_ScopedVMTInstall(ControlSubTask, 0x660D80, {.reversed = false});
 }
@@ -35,7 +38,25 @@ CTaskComplexSignalAtPed::~CTaskComplexSignalAtPed() {
 
 // 0x660B30
 CTask* CTaskComplexSignalAtPed::CreateNextSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x660B30, CTaskComplexSignalAtPed*, CPed*>(this, ped);
+    switch (m_pSubTask->GetTaskType()) {
+    case TASK_COMPLEX_TURN_TO_FACE_ENTITY: {
+        auto pause = m_initialPause;
+        if (pause == -1) {
+            pause = CGeneral::GetRandomNumberInRange(0, 2000);
+        }
+        return new CTaskSimpleStandStill(pause, false, false, 8.0f);
+    }
+    case TASK_SIMPLE_STAND_STILL:
+        return new CTaskSimpleDoHandSignal();
+    case TASK_SIMPLE_DO_HAND_SIGNAL:
+        if (m_areAnimsReferenced && m_playAnimAtEnd) {
+            return new CTaskSimpleRunAnim((AssocGroupId)0x34, (AnimationId)(0x117 + CGeneral::GetRandomNumberInRange(-16, 0)), 4.0f, false);
+        }
+        break;
+    default:
+        break;
+    }
+    return nullptr;
 }
 
 // 0x660CC0
