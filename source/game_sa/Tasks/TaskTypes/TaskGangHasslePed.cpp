2bb9fb9bfb8897afe9f47dee0ebdac1eaaf44d51
TheCrazy17 <fedehromero@outlook.com>
2026-09-07 16:20:02 -0300

    re(CTaskGangHasslePed): Reverse CreateNextSubTask, CreateFirstSubTask
    Also fixes a pre-existing draft bug: it constructed CEntitySeekPosCalculatorStandard
    (a helper struct, not a task) instead of CTaskComplexSeekEntityStandard.

--- source/game_sa/Tasks/TaskTypes/TaskGangHasslePed.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskGangHasslePed.cpp
@@ -10,8 +10,8 @@ void CTaskGangHasslePed::InjectHooks() {
     RH_ScopedInstall(Constructor, 0x65FED0);
     RH_ScopedInstall(Destructor, 0x65FF60);
 
-    RH_ScopedVMTInstall(CreateNextSubTask, 0x6642C0, { .reversed = false });
-    RH_ScopedVMTInstall(CreateFirstSubTask, 0x664380, { .reversed = false });
+    RH_ScopedVMTInstall(CreateNextSubTask, 0x6642C0);
+    RH_ScopedVMTInstall(CreateFirstSubTask, 0x664380);
     RH_ScopedVMTInstall(ControlSubTask, 0x65FFE0, { .reversed = false });
 }
 
@@ -39,33 +39,34 @@ CTaskGangHasslePed::~CTaskGangHasslePed() {
     }
 }
 
+namespace {
+auto& s_HassleMaxEntityDistMult = StaticRef<float>(0x86FC2C);
+auto& s_HassleMoveStateRadius   = StaticRef<float>(0x86FC28);
+auto& s_HassleFollowNodeThresholdHeightChange = StaticRef<float>(0x86FC30);
+}; // namespace
+
 // 0x6642C0
 CTask* CTaskGangHasslePed::CreateNextSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x6642C0, CTaskGangHasslePed*, CPed*>(this, ped);
-    /*
-    if (m_pSubTask->GetTaskType() == TASK_COMPLEX_KILL_PED_ON_FOOT)
+    if (m_pSubTask->GetTaskType() == TASK_COMPLEX_KILL_PED_ON_FOOT) {
         return nullptr;
+    }
 
-    auto radius = CGeneral::GetRandomNumberInRange(3.0f, 5.0f); // strange math here
-    return new CEntitySeekPosCalculatorStandard(m_Ped, 999'999, 1000, radius, 2.0f, 2.0f, false, true);
-    */
+    const auto radius = CGeneral::GetRandomNumberInRange(3.0f, 5.0f); // strange math here
+    return new CTaskComplexSeekEntityStandard(m_Ped, 999'999, 1000, s_HassleMaxEntityDistMult * radius, s_HassleMoveStateRadius, s_HassleFollowNodeThresholdHeightChange, false, true);
 }
 
 // 0x664380
 CTask* CTaskGangHasslePed::CreateFirstSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x664380, CTaskGangHasslePed*, CPed*>(this, ped);
-
-    /*
-    if (!m_Ped)
+    if (!m_Ped) {
         return nullptr;
+    }
 
-    m_spentWaitingMs = CTimer::GetTimeInMS();
+    const auto radius = CGeneral::GetRandomNumberInRange(3.0f, 5.0f); // strange math here
+    m_nTime = CTimer::GetTimeInMS();
     m_nSomeRandomShit = CGeneral::GetRandomNumberInRange(m_RndMin, m_RndMax);
     m_bFirstSubTaskInitialised = true;
 
-    auto radius = CGeneral::GetRandomNumberInRange(3.0f, 5.0f); // strange math here
-    return new CEntitySeekPosCalculatorStandard(m_Ped, 999'999, 1000, radius, 2.0f, 2.0f, false, true);
-    */
+    return new CTaskComplexSeekEntityStandard(m_Ped, 999'999, 1000, s_HassleMaxEntityDistMult * radius, s_HassleMoveStateRadius, s_HassleFollowNodeThresholdHeightChange, false, true);
 }
 
 // 0x65FFE0
