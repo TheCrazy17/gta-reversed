3abc8c3b559362a03c65b77a02ed0a1f90f8b2fe
TheCrazy17 <fedehromero@outlook.com>
2026-09-07 16:09:58 -0300

    re(CPedGroupDefaultTaskAllocatorRandom): Reverse AllocateDefaultTasks

--- source/game_sa/Tasks/Allocators/PedGroup/PedGroupDefaultTaskAllocatorRandom.h
+++ source/game_sa/Tasks/Allocators/PedGroup/PedGroupDefaultTaskAllocatorRandom.h
@@ -5,12 +5,33 @@
 #include <reversiblehooks/ReversibleHooks.h>
 
 #include "./PedGroupDefaultTaskAllocator.h"
+#include "TaskComplexGangFollower.h"
+#include "TaskComplexGangLeader.h"
+#include "TaskComplexFollowLeaderInFormation.h"
 
 class NOTSA_EXPORT_VTABLE CPedGroupDefaultTaskAllocatorRandom final : public CPedGroupDefaultTaskAllocator {
 public:
     /* no virtual destructor */
 
-    void                              AllocateDefaultTasks(CPedGroup* pedGroup, CPed* ped) const override { plugin::CallMethod<0x5F6E90>(this, pedGroup, ped); };
+    // 0x5F6E90
+    void AllocateDefaultTasks(CPedGroup* pedGroup, CPed* ped) const override {
+        auto* const leader = pedGroup->GetMembership().GetLeader();
+        auto& taskPairs = pedGroup->GetIntelligence().GetDefaultPedTaskPairs();
+        for (auto i = 0; i < CPedGroupMembership::LEADER_MEM_ID; i++) {
+            auto& tp = taskPairs[i];
+            if (!(tp.Ped && (!ped || tp.Ped == ped))) {
+                continue;
+            }
+            const auto& offs = CTaskComplexFollowLeaderInFormation::ms_offsets.Offsets[i];
+            auto* const task = new CTaskComplexGangFollower{ pedGroup, leader, (uint8)i, CVector{ offs.x, offs.y, 0.0f }, 10.0f };
+            task->m_Flags = (task->m_Flags & ~4) | (pedGroup->m_bMembersEnterLeadersVehicle ? 4 : 0);
+            tp.Task = task;
+        }
+        auto& leaderTp = taskPairs[CPedGroupMembership::LEADER_MEM_ID];
+        if (leaderTp.Ped && (!ped || leaderTp.Ped == ped)) {
+            leaderTp.Task = new CTaskComplexGangLeader{ pedGroup };
+        }
+    };
     ePedGroupDefaultTaskAllocatorType GetType() const override { return ePedGroupDefaultTaskAllocatorType::RANDOM; }; // 0x5F6530
 
 public:
@@ -18,7 +39,7 @@ public:
         RH_ScopedVirtualClass(CPedGroupDefaultTaskAllocatorRandom, 0x86C77C, 2);
         RH_ScopedCategory("Tasks/Allocators/PedGroup");
 
-        RH_ScopedVMTInstall(AllocateDefaultTasks, 0x5F6E90, { .reversed = false });
+        RH_ScopedVMTInstall(AllocateDefaultTasks, 0x5F6E90);
         RH_ScopedVMTInstall(GetType, 0x5F6530);
     }
 };
