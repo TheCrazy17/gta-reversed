2de34f36bb7aa032048c9bae0a56eea2eb6675df
TheCrazy17 <fedehromero@outlook.com>
2026-09-07 17:07:14 -0300

    re(CTaskComplexKillPedFromBoat): Reverse ControlSubTask

--- source/game_sa/Tasks/TaskTypes/TaskComplexKillPedFromBoat.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexKillPedFromBoat.cpp
@@ -1,6 +1,12 @@
 #include "StdInc.h"
 #include "TaskComplexKillPedFromBoat.h"
 #include "TaskSimpleStandStill.h"
+#include "TaskComplexKillPedOnFoot.h"
+#include "Entity/Vehicle/Vehicle.h"
+#include "Audio/AudioEngine.h"
+#include "Audio/Enums/PedSpeechContexts.h"
+#include "World.h"
+#include "Wanted.h"
 
 void CTaskComplexKillPedFromBoat::InjectHooks() {
     RH_ScopedVirtualClass(CTaskComplexKillPedFromBoat, 0x86da98, 11);
@@ -12,7 +18,7 @@ void CTaskComplexKillPedFromBoat::InjectHooks() {
     RH_ScopedVMTInstall(GetTaskType, 0x622820);
     RH_ScopedVMTInstall(CreateNextSubTask, 0x622890);
     RH_ScopedVMTInstall(CreateFirstSubTask, 0x622900);
-    RH_ScopedVMTInstall(ControlSubTask, 0x622980, { .reversed = false });
+    RH_ScopedVMTInstall(ControlSubTask, 0x622980);
 
 }
 
@@ -45,5 +51,34 @@ CTask* CTaskComplexKillPedFromBoat::CreateFirstSubTask(CPed* ped) {
 
 // 0x622980
 CTask* CTaskComplexKillPedFromBoat::ControlSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x622980, CTaskComplexKillPedFromBoat*, CPed*>(this, ped);
+    switch (m_pSubTask->GetTaskType()) {
+    case TASK_COMPLEX_KILL_PED_ON_FOOT: {
+        if (ped->IsCop() && m_Ped && m_Ped->IsPlayer()) {
+            if (const auto* const vehicle = m_Ped->GetVehicleIfInOne()) {
+                if (vehicle->m_vehicleAudio.m_AuSettings.VehicleAudioType == AE_BOAT) {
+                    AudioEngine.SayPedless(AE_SPEECH_PED, CTX_GLOBAL_POLICE_BOAT, ped, 0, 1.0f, false, false, false);
+                }
+            } else if (m_Ped->physicalFlags.bSubmergedInWater) {
+                AudioEngine.SayPedless(AE_SPEECH_PED, CTX_GLOBAL_POLICE_OVERBOARD, ped, 0, 1.0f, false, false, false);
+            }
+        }
+
+        if (ped->IsCop() && m_Ped && m_Ped->IsPlayer() && FindPlayerWanted()->GetWantedLevel() == eWantedLevel::WANTED_CLEAN) {
+            if (m_pSubTask->MakeAbortable(ped, ABORT_PRIORITY_URGENT, nullptr)) {
+                return new CTaskSimpleStandStill(0, true, false, 8.0f);
+            }
+        }
+        break;
+    }
+    case TASK_SIMPLE_STAND_STILL:
+        if (ped->IsCop() && m_Ped && m_Ped->IsPlayer() && FindPlayerWanted()->GetWantedLevel() != eWantedLevel::WANTED_CLEAN) {
+            if (m_pSubTask->MakeAbortable(ped, ABORT_PRIORITY_URGENT, nullptr)) {
+                return new CTaskComplexKillPedOnFoot(m_Ped, -1, 0, 0, 0, 1);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+    return m_pSubTask;
 }
