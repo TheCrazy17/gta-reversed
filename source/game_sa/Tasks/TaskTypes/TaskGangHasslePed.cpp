264c1ac03e8af2ddd6df2b6451f40b6c34b846cb
TheCrazy17 <fedehromero@outlook.com>
2026-09-08 10:39:29 -0300

    re(CTaskGangHasslePed): Reverse ControlSubTask

--- source/game_sa/Tasks/TaskTypes/TaskGangHasslePed.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskGangHasslePed.cpp
@@ -2,6 +2,10 @@
 #include "StdInc.h"
 
 #include "TaskGangHasslePed.h"
+#include "TaskComplexGangLeader.h"
+#include "TaskComplexKillPedOnFoot.h"
+#include "TaskComplexPlayHandSignalAnim.h"
+#include "TaskSimpleRunAnim.h"
 
 void CTaskGangHasslePed::InjectHooks() {
     RH_ScopedVirtualClass(CTaskGangHasslePed, 0x86FA00, 11);
@@ -12,7 +16,7 @@ void CTaskGangHasslePed::InjectHooks() {
 
     RH_ScopedVMTInstall(CreateNextSubTask, 0x6642C0);
     RH_ScopedVMTInstall(CreateFirstSubTask, 0x664380);
-    RH_ScopedVMTInstall(ControlSubTask, 0x65FFE0, { .reversed = false });
+    RH_ScopedVMTInstall(ControlSubTask, 0x65FFE0);
 }
 
 // 0x65FED0
@@ -43,6 +47,12 @@ namespace {
 auto& s_HassleMaxEntityDistMult = StaticRef<float>(0x86FC2C);
 auto& s_HassleMoveStateRadius   = StaticRef<float>(0x86FC28);
 auto& s_HassleFollowNodeThresholdHeightChange = StaticRef<float>(0x86FC30);
+
+// NOTSA: same list as CTaskComplexGangLeader's file-local s_gangTalkAnims (that one has internal linkage)
+constexpr AnimationId s_gangTalkAnims[]{
+    ANIM_ID_PRTIAL_GNGTLKA, ANIM_ID_PRTIAL_GNGTLKB, ANIM_ID_PRTIAL_GNGTLKC, ANIM_ID_PRTIAL_GNGTLKD,
+    ANIM_ID_PRTIAL_GNGTLKE, ANIM_ID_PRTIAL_GNGTLKF, ANIM_ID_PRTIAL_GNGTLKG, ANIM_ID_PRTIAL_GNGTLKH,
+};
 }; // namespace
 
 // 0x6642C0
@@ -71,5 +81,61 @@ CTask* CTaskGangHasslePed::CreateFirstSubTask(CPed* ped) {
 
 // 0x65FFE0
 CTask* CTaskGangHasslePed::ControlSubTask(CPed* ped) {
-    return plugin::CallMethodAndReturn<CTask*, 0x65FFE0, CTaskGangHasslePed*, CPed*>(this, ped);
+    if (!m_Ped) {
+        return nullptr;
+    }
+
+    if (m_bFirstSubTaskInitialised) {
+        if (m_bRefreshTime) {
+            m_nTime = CTimer::GetTimeInMS();
+            m_bRefreshTime = false;
+        }
+
+        if (m_nSomeRandomShit + m_nTime <= (int32)CTimer::m_snTimeInMilliseconds) {
+            if (dword10 == 2) {
+                if (m_pSubTask->GetTaskType() != TASK_COMPLEX_KILL_PED_ON_FOOT) {
+                    return new CTaskComplexKillPedOnFoot(m_Ped);
+                }
+            } else if (dword10 != 1) {
+                return nullptr;
+            }
+        }
+    }
+
+    // Keep the "gangs" (talk/hand-signal) animation block referenced only while it's actually needed
+    if (!m_bAnimBlockRefAdded) {
+        if (CTaskComplexGangLeader::ShouldLoadGangAnims()) {
+            const auto blk = CAnimManager::GetAnimationBlockIndex("gangs");
+            if (CAnimManager::GetAnimBlocks()[blk].IsLoaded) {
+                CAnimManager::AddAnimBlockRef(blk);
+                m_bAnimBlockRefAdded = true;
+            } else {
+                CStreaming::RequestModel(IFPToModelId(blk), STREAMING_KEEP_IN_MEMORY);
+            }
+        }
+    } else if (!CTaskComplexGangLeader::ShouldLoadGangAnims()) {
+        CAnimManager::RemoveAnimBlockRef(CAnimManager::GetAnimationBlockIndex("gangs"));
+        m_bAnimBlockRefAdded = false;
+    }
+
+    // Occasionally kick off a "partial anim" secondary task (talk anim or hand signal)
+    if (m_bAnimBlockRefAdded && !ped->IsPlayingHandSignal() && !ped->GetTaskManager().GetTaskSecondary(TASK_SECONDARY_PARTIAL_ANIM)) {
+        const auto rnd = CGeneral::GetRandomNumberInRange(0, 200);
+        if (51 <= rnd && rnd <= 55) { // 2.5% chance
+            ped->GetTaskManager().SetTaskSecondary(
+                new CTaskSimpleRunAnim(ANIM_GROUP_GANGS, CGeneral::RandomChoice(s_gangTalkAnims), 4.f, false),
+                TASK_SECONDARY_PARTIAL_ANIM
+            );
+        } else if (rnd == 100) { // 0.5% chance
+            ped->GetTaskManager().SetTaskSecondary(new CTaskComplexPlayHandSignalAnim(), TASK_SECONDARY_PARTIAL_ANIM);
+        }
+    }
+
+    if (dword10 == 0) {
+        ped->Say(CTX_GLOBAL_EYEING_PED, 0, 1.f, false, false, false);
+    } else if (dword10 == 1 || dword10 == 2) {
+        CTaskComplexGangLeader::DoGangAbuseSpeech(ped, m_Ped);
+    }
+
+    return m_pSubTask;
 }
